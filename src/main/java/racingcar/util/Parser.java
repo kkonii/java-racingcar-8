e656da3033d5da528085a2bdf31ccd558ae88c5f
kkonii <119216472+kkonii@users.noreply.github.com>
2025-10-27 01:22:47 +0900

    refactor: 이름을 분리하는 기능의 위치 및 메서드명 수정
    - domain 의 핵심과 관련 없는 단순 입력값 처리이므로 Parser 로 이동

--- src/main/java/racingcar/util/Parser.java
+++ src/main/java/racingcar/util/Parser.java
@@ -1,14 +1,18 @@
 package racingcar.util;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import racingcar.exception.Error;
 
 public class Parser {
 
     private static final Pattern NUMERIC = Pattern.compile("^[+-]?\\d+$");
+    private static final String DELIMITER = ",";
 
-    public static int parseToInteger(String value) {
+    public static int toInteger(String value) {
         validateType(value);
 
         return Integer.parseInt(value);
@@ -20,5 +24,11 @@ public class Parser {
         if (!matcher.matches()) {
             throw new IllegalArgumentException(Error.NOT_A_NUMBER.message());
         }
+    }
+
+    public static List<String> toNames(String consoleInput) {
+        return Arrays.stream(consoleInput.split(DELIMITER))
+                .map(String::strip)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/racingcar/util/ParserTest.java
+++ src/test/java/racingcar/util/ParserTest.java
@@ -1,7 +1,9 @@
 package racingcar.util;
 
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -16,7 +18,7 @@ public class ParserTest {
         String notNumeric = "baka";
 
         //then
-        Assertions.assertThrows(IllegalArgumentException.class, () -> Parser.parseToInteger(notNumeric));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Parser.toInteger(notNumeric));
     }
 
     @ParameterizedTest
@@ -24,6 +26,17 @@ public class ParserTest {
     @DisplayName("[성공] 숫자로만 이루어진 문자열을 변환하는 데에 성공한다")
     void test_parsing_numeric(String consoleInput, int parsedInput) {
         //then
-        Assertions.assertEquals(Parser.parseToInteger(consoleInput), parsedInput);
+        Assertions.assertEquals(Parser.toInteger(consoleInput), parsedInput);
+    }
+
+    @Test
+    @DisplayName("[성공] 지정된 구분자로 정확하게 이름을 구분하는 데에 성공한다")
+    void test_separate_name_without_delimiter() {
+        //given
+        String consoleInput = "포비, 워니 ,준";
+        List<String> names = Parser.toNames(consoleInput);
+
+        //then
+        org.assertj.core.api.Assertions.assertThat(names).containsExactly("포비", "워니", "준");
     }
 }
